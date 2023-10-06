e2ab61629e00b7f87024fd179b9afbcb672b92bf
Myo Myint Aung <myomyintaung064@gmail.com>
2023-10-06 10:06:29 +0630

    code fixes

---
+++ src/core/color.js
@@ -0,0 +1,8 @@
+export default {
+  primaryColor: "#34495E",
+  linkColor: "#16A085",
+  headerBgColor: "#34495E",
+  whiteColor: "#FFFFFF",
+  textColor: "#A9A9A9",
+  activeShadowColor: "0 0 0 2.5px rgba(5, 145, 255, 0.1)",
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { ConfigProvider } from 'antd'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ConfigProvider theme={{
-        token: {
-          colorPrimary: "#34495E",
-          colorLink: "#16A085",
-          colorBgHeader: "#34495E",
-          colorBgLayout: "#FFFFFF",
-          colorText: "#A9A9A9",
-          fontFamily: `"Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"`       
-        },
-        components: {
-          Button: {
-            primaryShadow: "none"
-          },
-          Input: {
-            activeShadow: "0 0 0 2.5px rgba(5, 145, 255, 0.1)"
-          }
-        }
-      }}>
-        <App />
-      </ConfigProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
